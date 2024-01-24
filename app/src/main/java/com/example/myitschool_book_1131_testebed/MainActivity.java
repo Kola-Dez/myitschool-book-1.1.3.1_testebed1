796d20de32629caa9bd8bf41cf88fdf3eff97b81
Kola-Dez <kola.stalker.2004@gmail.com>
2024-01-24 08:43:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myitschool_book_1131_testebed/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myitschool_book_1131_testebed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "myitschool-book-1.1.3.1_testebed"
+include(":app")
